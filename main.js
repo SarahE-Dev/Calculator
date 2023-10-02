a60ef44b28c089cba5d6c8bdb29ba21005c72806
Sarah E <seatherly.prsvr@gmail.com>
2023-10-02 09:53:18 -0500

    keypress color edit

--- main.js
+++ main.js
@@ -9,73 +9,187 @@ let calculator = document.querySelector('.calculator');
 let buttons = document.querySelectorAll('button');
 let label = document.querySelector('label');
 
+function keydownColor(i){
+    buttons[i].style.background = 'white';
+    buttons[i].style.color = 'deeppink';
+}
+
+function colorback(){
+    if(checkbox.checked){
+        buttons.forEach((item)=>{
+            item.style.background = 'linear-gradient(to bottom, darkorchid, royalblue, darkmagenta)'
+            item.style.color = 'white';
+        })
+    }else{
+        buttons.forEach((item)=>{
+            item.style.background = 'linear-gradient(to bottom, royalblue, deeppink)';
+            item.style.color = 'white';
+        })
+    }
+}
+    
+
 window.addEventListener('keydown', (event)=>{
     switch(event.key){
         case '1':
             screen(1);
+            keydownColor(4);
             break;
         case '2':
             screen(2);
+            keydownColor(5);
             break;
         case '3':
             screen(3);
+            keydownColor(6);
             break;
         case '4':
             screen(4);
+            keydownColor(8);
             break;
         case '5':
             screen(5);
+            keydownColor(9);
             break;
         case '6':
             screen(6);
+            keydownColor(10);
             break;
         case '7':
             screen(7);
+            keydownColor(12);
             break;
         case '8':
             screen(8);
+            keydownColor(13);
             break;
         case '9':
             screen(9);
+            keydownColor(14);
             break;
         case '0':
             screen(0);
+            keydownColor(17);
             break;
         case '.':
             screen('.');
+            keydownColor(18);
             break;
         case '*':
             operator('*');
+            keydownColor(7);
             break;
         case '/':
             operator('/');
+            keydownColor(3);
             break;
         case '+':
             operator('+');
+            keydownColor(15);
             break;
         case '-':
             operator('-');
+            keydownColor(11);
             break;
         case 'Backspace':
             cancel();
+            keydownColor(1);
             break;
         case '=':
             exe('=');
+            keydownColor(19);
             break;
         case 'Enter':
             exe('=');
+            keydownColor(19);
             break;
         case 'Delete':
             cls();
+            keydownColor(0);
             break;
         case 'Escape':
             cls();
+            keydownColor(0);
             break;
         case '%':
             percentage('%');
+            keydownColor(16);
             break;
         case ';':
             squareRoot();
+            keydownColor(2);
+            break;
+        
+    }
+})
+
+window.addEventListener('keyup', (event)=>{
+    switch(event.key){
+        case '1':
+            colorback();
+            break;
+        case '2':
+            colorback();
+            break;
+        case '3':
+            colorback();
+            break;
+        case '4':
+            colorback();
+            break;
+        case '5':
+            colorback();
+            break;
+        case '6':
+            colorback();
+            break;
+        case '7':
+            colorback();
+            break;
+        case '8':
+            colorback();
+            break;
+        case '9':
+            colorback();
+            break;
+        case '0':
+            colorback();
+            break;
+        case '.':
+            colorback();
+            break;
+        case '*':
+            colorback();
+            break;
+        case '/':
+            colorback();
+            break;
+        case '+':
+            colorback();
+            break;
+        case '-':
+            colorback();
+            break;
+        case 'Backspace':
+            colorback();
+            break;
+        case '=':
+            colorback();
+            break;
+        case 'Enter':
+            colorback();
+            break;
+        case 'Delete':
+            colorback();
+            break;
+        case 'Escape':
+            colorback();
+            break;
+        case '%':
+            colorback();
+            break;
+        case ';':
+            colorback();
             break;
         
     }
